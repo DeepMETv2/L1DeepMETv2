2d2c1174c26b83a10cde94b6e729dd51cc60b042
Kyungmin Park <kyungmin.park@cern.ch>
2023-07-10 12:21:11 -0400

    update filelist for ttbar

--- data_ttbar/generate_npz.sh
+++ data_ttbar/generate_npz.sh
@@ -1,1 +1,2 @@
-nohup python -u generate_npz.py -d TTbar -s 0 -e 1 TTbar.log 
+#python -u generate_npz.py -d TTbar -s 0 -e 1 TTbar.log 
+nohup python -u generate_npz.py -d TTbar -s 0 -e 7 > TTbar.log 
