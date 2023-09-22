6b1aa9210f0d32630040998345afb64e61e345b2
Kyungmin Park <kyungmin.park@cern.ch>
2023-09-22 15:29:58 -0400

    update yml, removing hash from yml

--- load.py
+++ load.py
@@ -1,7 +1,7 @@
 import torch
 from torch_geometric.utils import to_undirected
 
-model_dir = '/export/home/phys/kyungmip/L1DeepMETv2/ckpts_sep21/'       # name of the ckpts folder'
+model_dir = './ckpts_sep21/'       # name of the ckpts folder'
 
 loaded_model = torch.jit.load(f'{model_dir}/scripted_model.pt')
 
