8b217334aab95e4b72759432abce4e2167cfdc9e
Sena Durgut <abrekk01@gmail.com>
2024-04-11 12:47:43 -0400

    Update net.py

--- model/net.py
+++ model/net.py
@@ -29,7 +29,6 @@ class Net(nn.Module):
         relu_layer = nn.ReLU()
         return relu_layer(weights)
         #return torch.sigmoid(weights)
-        return F.relu(weights)
 
 # tensor operations
 def getdot(vx, vy):
